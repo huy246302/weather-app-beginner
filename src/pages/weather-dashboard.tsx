34ca38c72630bfb83e79e5c24466f29d7ab5f626
Nguyen Huy <huy246302@gmail.com>
2025-08-17 21:37:56 +0700

    5-days-forecast

--- src/pages/weather-dashboard.tsx
+++ src/pages/weather-dashboard.tsx
@@ -7,6 +7,7 @@ import { useForecastQuery, useReverseGeocodeQuery, useWeatherQuery } from "@/hoo
 import CurrentWeather from "@/components/current-weather";
 import HourlyTemperature from "@/components/hourly-temperature";
 import WeatherDetails from "@/components/weather-details";
+import WeatherForecast from "@/components/weather-forecast";
 
 const WeatherDashboard = () => {
   const { 
@@ -115,8 +116,9 @@ const WeatherDashboard = () => {
             <HourlyTemperature data={forecastQuery.data}/>
         </div>
 
-        <div>
+        <div className="grid gap-6 md:grid-cols-2 items-start">
           <WeatherDetails data={weatherQuery.data}/>
+          <WeatherForecast data={forecastQuery.data}/>
         </div>
       </div>
     </div>
