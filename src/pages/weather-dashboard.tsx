0ebacb86373986985d5f86726f753d0d216a91d8
Nguyen Huy <huy246302@gmail.com>
2025-08-27 14:46:08 +0700

    syntax

--- src/pages/weather-dashboard.tsx
+++ src/pages/weather-dashboard.tsx
@@ -91,7 +91,7 @@ const WeatherDashboard = () => {
   }
 
   return (
-    <div className="sapce-y-4">
+    <div className="space-y-4">
       <FavoriteCities />
       <div className="flex items-center justify-between">
         <h1 className="text-xl font-bold tracking-tight">My Location</h1>
